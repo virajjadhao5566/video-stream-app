644b4e41e517d36ae320076a3a34deb8fe0a41ab
Viraj <virajjadhao85@gmail.com>
2023-09-14 23:34:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Welcome to Cloud Streamer</h1>
+      <video width="700px" height="400px" controls>
+        <source src='https://d3kdcam0njjw2f.cloudfront.net/videoplayback.mp4' type='video/mp4'/>
+      </video>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
