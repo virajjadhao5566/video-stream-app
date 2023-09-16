da23ae6af8a53f941bd5d9ee4d31f6f596a33442
Viraj <virajjadhao85@gmail.com>
2023-09-16 17:58:37 +0530

    name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,6 @@ const Navbar = () => (
   <Stack className="nav" direction="row" alignItems="center" p={1} sx={{ position: "sticky", top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
-      <h1 style={{ color:"white" ,marginLeft:"3px"}}>Xtrim</h1>
     </Link>
     <SearchBar />
   </Stack>
